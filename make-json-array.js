24e07ecca0be957afd460ee5de22a247ee49fda6
mikelothar <mikelothar@gmail.com>
2016-05-26 22:08:47 +0200

    save array to file

--- make-json-array.js
+++ make-json-array.js
@@ -1,4 +1,5 @@
-var request = require("request")
+var request = require("request"),
+    fs = require('fs');
 
 var urls = [
   "https://raw.githubusercontent.com/lthr/livesearch-jsons/master/mock/mock1.json",
@@ -6,30 +7,22 @@ var urls = [
   "https://raw.githubusercontent.com/lthr/livesearch-jsons/master/mock/mock3.json"
 ];
 
-var output = [];
-var count = 0;
+var output = [],
+    count = 0;
 
 for (var url in urls) {
-
   (function (url) {
     request({
       url: urls[url],
       json: true
     }, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        //console.log(body); // Print the json response
         output.push(body);
         count++;
       }
-
       if (count == urls.length) {
-        console.log(output);
+        fs.writeFile('./data.js', JSON.stringify(output), console.log('Saved!'));
       }
-
     });
   })(url);
-
-
 }
-
-
